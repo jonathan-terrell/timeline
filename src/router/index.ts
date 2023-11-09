4d35d18a2efd7f9834aead2b510c2116ee219e76
Jonathan Terrell <terrell.jm@gmail.com>
2023-11-09 21:46:45 +1100

    updates

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 import GridView from '../views/GridView.vue';
+import VisView from '../views/VisView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
         { path: '/', name: 'home', component: HomeView },
-        { path: '/grid', name: 'grid', component: GridView }
+        { path: '/grid', name: 'grid', component: GridView },
+        { path: '/vis', name: 'vis', component: VisView }
     ]
 });
 
